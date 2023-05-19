e70dc7907adca16abc9c1b76f51c188b70034f05
Gabriel <murariugabi23@gmail.com>
2023-05-19 18:41:45 +0300

    Am terminat prima tema la JS

---
+++ Homework1-Arrays/index.js
@@ -0,0 +1,119 @@
+const initialValues = [NaN, null, undefined, "string", 1, false]
+
+const otherValues = [
+  undefined,
+  null,
+  1,
+  false,
+  "string",
+  ["tema", false],
+  true,
+  10,
+]
+
+
+
+                                    // TEMA JS ARRAY
+
+// initialValues.push(otherValues[0])
+// initialValues.push(otherValues[1])
+// initialValues.push(otherValues[2])
+// initialValues.push(otherValues[3])
+// initialValues.push(otherValues[4])
+// initialValues.push(otherValues[5])
+// initialValues.push(otherValues[6])
+// initialValues.push(otherValues[otherValues.length - 1])
+
+// const adunare = initialValues[4] + initialValues[initialValues.length - 1]
+// const scadere = initialValues[initialValues.length - 1] - 5
+
+// initialValues[initialValues.length - 1] = scadere
+// initialValues[4] = adunare
+
+// initialValues[3] = initialValues[3] + " HappyCoding"
+// initialValues[10] = initialValues[10] + " HappyCoding"
+// initialValues[11][0] = initialValues[11][0] + " HappyCoding"
+
+// initialValues[11][1] = !initialValues[11][1]
+// initialValues[5] = !initialValues[5]
+// initialValues[9] = !initialValues[9]
+// initialValues[12] = !initialValues[12]
+
+// console.log(initialValues)
+
+
+                                    // ALTE INCERCARI
+
+                                    // REGULAR FOR
+
+// for(let i = 0; i < otherValues.length; i++){
+//   initialValues.push(otherValues[i])
+// }
+
+// for (let i = 0; i < initialValues.length; i++) {
+//   if (typeof initialValues[i] === "number") {
+//     initialValues[i] = initialValues[i] + 2
+//   }
+
+//   if (typeof initialValues[i] === "string") {
+//     initialValues[i] = initialValues[i] + " happy coding"
+//    }
+
+//   if (typeof initialValues[i] === "boolean") {
+//     initialValues[i] = !initialValues[i]
+//   }
+
+//   if (typeof initialValues[i] === "object" && initialValues[i] != null) {
+//     for(let j = 0; j < initialValues[i].length; j++){
+//       if(typeof initialValues[i][j] === "boolean"){
+//         initialValues[i][j] = !initialValues[i][j]
+//       }
+//       if (typeof initialValues[i][j] === "boolean") {
+//         initialValues[i][j] = !initialValues[i][j]
+//       }if(typeof initialValues[i][j] === "string"){
+//         initialValues[i][j] = initialValues[i][j] + " happy coding"
+//       }
+//     }
+//   }
+// }
+
+// console.log(initialValues)
+
+
+                                  // FOR OF
+
+// for (let element of otherValues) {
+//   initialValues.push(element)
+// }
+
+// console.log("Before \n", initialValues)
+
+// for (const [index, element] of initialValues.entries()) {
+//   if (typeof element === "number") {
+//     initialValues[index] = element + 2
+//   }
+
+//   if (typeof element === "string") {
+//     initialValues[index] = element + " happy coding"
+//   }
+
+//   if (typeof element === "boolean") {
+//     initialValues[index] = !element
+//   }
+
+// // Nu stiu de unde am nascocit aceasta incercare de for in for. Acum chiar ma intreb daca e ok
+
+//   if (typeof element === "object" && element != null) {
+//     for (const [i, el] of element.entries()) {
+//       if (typeof el === "string") {
+//         element[i] = el + " happy conding"
+//       }
+//       if (typeof el === "boolean") {
+//         element[i] = !el
+//       }
+//     }
+//   }
+// }
+
+// console.log(`After \n`, initialValues)
+
---
+++ README.md
@@ -0,0 +1,24 @@
+# Lucruri noi pe care le-am invatat facand temele
+
+## .entries()
+
+- .entries() poate fi folosit si la array-uri
+- Acesta returneaza un obiect iterator care ne permite sa iteram prin perechi de cheie/valoare
+
+```Js
+for (const [index, element] of array.entries()) {
+    console.log(element, index)
+}
+```
+
+## IndexOf() / chichite
+
+- .indexOf() ne permite sa gasim indexul unul element dintr-un array DAR daca array-ul nostru contine elemente la fel acesta ne va returna indexul primului element care se afla de mai multe ori in acesta
+
+
+
+
+
+
+
+
