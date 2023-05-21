82420c44adbf346a7c03ad121d580b74b2f26865
Gabriel <murariugabi23@gmail.com>
2023-05-21 14:24:54 +0300

    Am inceput tema

---
+++ HomeWork2-JS-Control-Flow/script.js
@@ -0,0 +1,86 @@
+const vehicul = prompt("Te rog introdu tipul de vehicul")
+const capacitate = prompt("Te rog adauga capacitatea cilindrinca")
+
+// initial cred ca as combina switch cu niste if-uri pe ici pe colo
+
+
+
+// Deci inputul meu este data introdusa de utilizator apoi
+// TREBUIE SA FAC CEVA CA IN CAZUL IN CARE InPUTUL MEU NU ESTE NUMAR SA IL refuze si sa apara din nou prompt-ul
+
+// Mai intai cer tipul de vehicul
+// apoi cer capacitatea cilindrica
+
+// ce face algoritmul?
+// imi preia inputul
+// verifica ce fel de vehicul am introdus
+// face calculul(in functie de vehicul la fiecare 200cm3 se aduna o suma in functie de vehihul )
+// apoi imi returneaza calculul
+//
+//
+//
+//
+//
+//
+
+// Output prin consola si un alert
+
+
+ const cm3PerCapacitate = capacitate / 200 
+
+
+ if(vehicul === "autoturism"){
+    alert(cm3PerCapacitate * 8)
+ }
+
+// alert(cm3PerCapacitate);
+
+// pentru autoturisme pana la 1600 cm3 moticiclete tricicluri si cvadriciclu
+const aPerCm3 = 8
+
+// pentru moticiclete tricicluri si cvadriciclu de peste la 1600 cm3 
+
+const moto = 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
