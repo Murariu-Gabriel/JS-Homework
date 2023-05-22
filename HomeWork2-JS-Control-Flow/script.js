75c4a47089733801e4874dd707b0e0f6e37fd5ef
Gabriel <murariugabi23@gmail.com>
2023-05-22 11:34:08 +0300

    Am terminat tema

--- HomeWork2-JS-Control-Flow/script.js
+++ HomeWork2-JS-Control-Flow/script.js
@@ -1,87 +1,84 @@
 const vehicul = prompt("Te rog introdu tipul de vehicul")
-const capacitate = prompt("Te rog adauga capacitatea cilindrinca")
-
-// initial cred ca as combina switch cu niste if-uri pe ici pe colo
-
-
-// LA TEMA ASTA TREBUIE SA FACI SI O VARIANTA CU HTML SI CSS INTRU-UN FORM
-
-// Deci inputul meu este data introdusa de utilizator apoi
-// TREBUIE SA FAC CEVA CA IN CAZUL IN CARE InPUTUL MEU NU ESTE NUMAR SA IL refuze si sa apara din nou prompt-ul
-
-// Mai intai cer tipul de vehicul
-// apoi cer capacitatea cilindrica
-
-// ce face algoritmul?
-// imi preia inputul
-// verifica ce fel de vehicul am introdus
-// face calculul(in functie de vehicul la fiecare 200cm3 se aduna o suma in functie de vehihul )
-// apoi imi returneaza calculul
-//
-//
-//
-//
-//
-//
-
-// Output prin consola si un alert
+const capacitate = parseInt(prompt("Te rog adauga capacitatea cilindrinca"))
+const caseProofVehicle = vehicul.toLowerCase()
 
 
- const cm3PerCapacitate = capacitate / 200 
+const cm3Division = capacitate / 200
 
-
- if(vehicul === "autoturism"){
-    alert(cm3PerCapacitate * 8)
- }
-
-// alert(cm3PerCapacitate);
-
-// pentru autoturisme pana la 1600 cm3 moticiclete tricicluri si cvadriciclu
-const aPerCm3 = 8
-
-// pentru moticiclete tricicluri si cvadriciclu de peste la 1600 cm3 
-
-const moto = 9
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+switch (caseProofVehicle) {
+  case "motocicleta":
+  case "cvadriclu":
+  case "triciclu": {
+    if (capacitate <= 1600) {
+      alert(`${Math.floor(cm3Division * 8)} lei`)
+      console.log("sub 1600")
+    }
+    else if (capacitate > 1600) {
+      alert(`${Math.floor(cm3Division * 9)} lei`)
+      console.log("Peste 1600")
+    } 
+    else {
+      alert("aceasta capacitate nu exista")
+    }
+    break
+  }
+  case "autoturism": {
+    if (capacitate <= 1600) {
+      alert(`${Math.floor(cm3Division * 8)} lei`)
+      console.log("sub 1600 auto")
+    }
+    else if (capacitate > 1600 && capacitate <= 2000) {
+      alert(`${Math.floor(cm3Division * 22)} lei`)
+      console.log("intre 1601 si 2000 auto")
+    }
+    else if (capacitate > 2001 && capacitate <= 2600) {
+      alert(`${Math.floor(cm3Division * 85)} lei`)
+      console.log("intre 2001 si 2601 auto")
+    }
+    else if (capacitate > 2601 && capacitate <= 3000) {
+      alert(`${Math.floor(cm3Division * 171)} lei`)
+      console.log("intre 2601 si 3000 auto")
+    }
+    else if (capacitate > 3001) {
+      alert(`${Math.floor(cm3Division * 345)} lei`)
+      console.log("peste 3001 auto")
+    } else {
+      alert("aceasta capacitate nu exista")
+    }
+    break
+  }
+  case "autobuz":
+  case "autocar":
+  case "microbuz": {
+    alert(
+      capacitate
+        ? `${Math.floor(cm3Division * 28)} lei`
+        : "Aceasta capacitate nu exista"
+    )
+    console.log("transport in comun")
+    break
+  }
+  case "tractor": {
+    alert(
+      capacitate
+        ? `${Math.floor(cm3Division * 22)} lei`
+        : "Aceasta capacitate nu exista"
+    )
+    console.log("tractor")
+    break
+  }
+  // Aici am decis sa prescurtez denumirea
+  case "camion": {
+    alert(
+      capacitate
+        ? `${Math.floor(cm3Division * 34)} lei`
+        : "Aceasta capacitate nu exista"
+    )
+    console.log("camion")
+    break
+  }
+  default: {
+    alert(`${caseProofVehicle} Acest vehicul nu exista`)
+    console.log(typeof capacitate)
+  }
+}
