c6ce6b76faa44768aabac2276be3890f8f72bcb8
Gabriel <murariugabi23@gmail.com>
2023-05-25 21:50:44 +0300

    Am terminat tema

---
+++ HomeWork-JS-Functions/script.js
@@ -0,0 +1,94 @@
+const firstSentence = `Ion castiga 5000 lei/luna din salariu, 10000 lei/an bonus si 1500 lei/luna din freelancing`
+
+const secondSentence = `Andrei are un salariu de 1000 euro/luna un bonus de 9000 lei/an si mai castiga suplimentar aproximativ 500 usd/luna din freelancing`
+
+const mockSentence = "Lucian are salar de 1000 de euro/luna un bonus de 10000 lei/an si castiga din freeelancing 3000 de lei/luna"
+
+const mockSentence2 = "Maria are salar de 1000 de usd/luna un bonus de 10000 lei/an si castiga din freeelancing 3000 de lei/luna"
+
+const mockSentence3 ="Darius are salar de 1000 de usd/luna un bonus de 10000 euro/an si castiga din freeelancing 3000 de usd/luna"
+
+
+
+const isStringMoney = (word) => {
+  if (word.includes("usd") || word.includes("euro") || word.includes("lei")) {
+    return true
+  }
+}
+
+const moneyTypesFroString = (array) => {
+  const money = []
+  for (const el of array) {
+    if (isStringMoney(el)) {
+      money.push(el)
+    }
+  }
+  return money
+}
+
+const numsFromString = (array) => {
+  const nums = []
+  for (const el of array) {
+    if (!isNaN(el)) {
+      nums.push(parseInt(el))
+    }
+  }
+  return nums
+}
+
+const calculTotal = (array) => {
+  const salarAnual = array[0] * 12
+  const bonus = array[1]
+  const extra = array[2] * 12
+  const castigAnual = salarAnual + bonus + extra
+
+  return castigAnual
+}
+
+const convertMoney = (sum, moneyType) => {
+    if(moneyType.includes("usd")){
+      return sum * 4.62
+    }
+     if (moneyType.includes("euro")) {
+       return sum * 4.95
+     }
+     return sum
+}
+
+const computeIncome = (sentence) => {
+  const splitSentence = sentence.split(" ")
+  const name = splitSentence[0]
+
+  const nums = numsFromString(splitSentence)
+  const total = calculTotal(nums)
+    // partea asta cauzeaza probleme
+  if (!splitSentence.includes("euro/luna") && !splitSentence.includes("usd/luna")) {
+    return `Venitul anual al lui ${name} este de ${total}`
+    
+  } else {
+    const moneyTypes = moneyTypesFroString(splitSentence)
+
+    for(const [index, element] of moneyTypes.entries()){
+       nums[index] = convertMoney(nums[index], element)
+       console.log(element);
+    }
+    
+    return `Venitul anual al lui ${name} este de ${calculTotal(nums)}`
+  }
+  
+}
+
+const ionIncome = computeIncome(firstSentence)
+const andreiIncome = computeIncome(secondSentence)
+const lucianIncome = computeIncome(mockSentence)
+const mariaIncome = computeIncome(mockSentence2)
+const dariusIncome = computeIncome(mockSentence3)
+
+console.log(ionIncome)
+console.log(andreiIncome)
+console.log(lucianIncome)
+console.log(mariaIncome)
+console.log(dariusIncome);
+
+console.log(isNaN("0")) // false
+console.log(isNaN("string")) // true
---
+++ README.md
@@ -0,0 +1,8 @@
+## verificarea unui numar intr-un string cu isNan
+
+- cu operatorul ! (not) pot sa imi fac viata mai usoara si sa il folosesc intr-o conditie ca sa verific daca un string este numar
+
+```Js
+console.log(isNaN("0")) // false
+console.log(isNaN("string")) // true
+```
